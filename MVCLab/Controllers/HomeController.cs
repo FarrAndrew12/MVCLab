750cff488779f4227e2c1ae4113aadde72aed576
FarrAndrew12 <101841142+FarrAndrew12@users.noreply.github.com>
2022-05-13 08:36:36 -0400

    first commit

---
+++ MVCLab/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLab.Models;
+using System.Diagnostics;
+
+namespace MVCLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        public IActionResult RegForm()
+        {
+            return View();
+        }
+       public IActionResult UserReg(UserReg Ur)
+        {
+            return View(Ur);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
